bb131afad71d7f69792db9e55f6a8fbc92e5087a
ArtSatanov <artsatanov@gmail.com>
2024-02-23 02:31:40 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { Routes } from './Routes/Routes';
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes />
+    </>
+  );
 };
