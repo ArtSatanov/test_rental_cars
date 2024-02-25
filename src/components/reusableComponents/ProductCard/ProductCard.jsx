d85e4dc1fde8e5e33f17e22a8e2ba332b60dad4d
ArtSatanov <artsatanov@gmail.com>
2024-02-25 23:17:20 +0200

    save

--- src/components/reusableComponents/ProductCard/ProductCard.jsx
+++ src/components/reusableComponents/ProductCard/ProductCard.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  Box,
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Typography,
-} from '@mui/material';
+import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
 
 import NoImg from '../../../images/no-image.jpg';
 import { CustomBtn } from '../Button/CustomBtn';
@@ -20,7 +13,7 @@ export const ProductCard = ({
   img,
   description,
   fuelConsumption,
-  engineSize,
+  engineSize = '3232',
   accessories,
   functionalities,
   rentalPrice = '$55',
@@ -64,28 +57,78 @@ export const ProductCard = ({
               display: 'flex',
               direction: 'row',
               justifyContent: 'space-between',
+              marginBottom: '8px',
             }}
           >
             <Box component="span">
               {make}
               <Box component="span" sx={{ color: 'primary.button' }}>
-                {' '}
-                {model}
-                {', '}
+                {` ${model}, `}
               </Box>
               {year}
             </Box>
             <Box component="span">{rentalPrice}</Box>
           </Typography>
-          <Typography variant="desc1" component="p">
-            {address.split(',')[1]}
-            {address.split(',')[2]}
-            {rentalCompany}
+          <Typography
+            variant="desc1"
+            component="p"
+            sx={{
+              marginBottom: '4px',
+              '& >*': {
+                borderRight: '1px solid rgba(18, 20, 23, 0.1)',
+                padding: '0 6px',
+              },
+            }}
+          >
+            <Box
+              component="span"
+              sx={{
+                paddingLeft: '0',
+              }}
+            >
+              {address.split(',')[1]}
+            </Box>
+            <Box component="span">{address.split(',')[2]}</Box>
+            <Box
+              component="span"
+              sx={{
+                borderRight: 'none !important',
+                paddingLeft: '6px',
+              }}
+            >
+              {rentalCompany}
+            </Box>
           </Typography>
-          <Typography variant="desc1" component="p">
-            {type}
-            {id}
-            {rentalCompany}
+          <Typography
+            variant="desc1"
+            component="p"
+            sx={{
+              marginBottom: '4px',
+              '& >*': {
+                borderRight: '1px solid rgba(18, 20, 23, 0.1)',
+                padding: '0 6px',
+              },
+            }}
+          >
+            <Box
+              component="span"
+              sx={{
+                paddingLeft: '0',
+              }}
+            >
+              {type}
+            </Box>
+            <Box component="span">{make}</Box>
+            <Box component="span">{id}</Box>
+            <Box
+              component="span"
+              sx={{
+                borderRight: 'none !important',
+                paddingLeft: '6px',
+              }}
+            >
+              {engineSize}
+            </Box>
           </Typography>
         </CardContent>
         <CustomBtn variant="learnMore">Learn More</CustomBtn>
